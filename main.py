2f0fb845782cd4d9a34fd3c1fb0dda09a2531944
Ezra <ezramalewis@gmail.com>
2022-11-28 19:10:20 -0500

    Import CSV data to table

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import sqlite3
+import pandas
 from pathlib import Path
 
 
-def main():
+def createDB():
     Path('data.db').touch()
     dataConn = sqlite3.connect('data.db')
     d = dataConn.cursor()
     d.execute(
-        """CREATE TABLE data (
+        """CREATE TABLE mxmh (
             time_taken datetime, age int,
             stream_serv text, daily int,
             while_working text, instrumentalist text,
             composer text, fav_genre text,
-            exporatory text, foreign_m text,
+            exploratory text, foreign_m text,
             bpm int, classical text,
             country text, edm text,
             folk text, gospel text,
@@ -27,6 +28,19 @@ def main():
             permissions text
         );"""
     )
+
+
+def main():
+    if not Path('data.db').exists():
+        createDB()
+
+    dbConn = sqlite3.connect('data.db')
+    d = dbConn.cursor()
+    
+    data = pandas.read_csv('mxmh_survey_results.csv')
+    data.to_sql('mxmh', dbConn, if_exists='append', index=False)
+
+
 
 
 if __name__ == "__main__":
